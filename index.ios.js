a37c92d1bde30b68bbb7e523d92141cb4a7eeca6
Adrian Stabiszewski <as@grundid.de>
2015-08-20 12:25:59 +0200

    map added

--- index.ios.js
+++ index.ios.js
@@ -1,59 +1,126 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    MapView,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    } = React;
 
-var kastanienApp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! Hello CoWo
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+var regionText = {
+    latitude: '0',
+    longitude: '0',
+    latitudeDelta: '0',
+    longitudeDelta: '0',
+};
+
+var MapViewExample = React.createClass({
+
+    getInitialState() {
+        return {
+            mapRegion: null,
+            mapRegionInput: null,
+            annotations: null,
+            isFirstLoad: true,
+        };
+    },
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (initialPosition) => this.didGetPosition(initialPosition.coords.latitude, initialPosition.coords.longitude),
+            (error) => alert(error.message),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+        );
+    },
+
+    didGetPosition(latitude, longitude) {
+        fetch('http://api.grundid.de/tree/near?latitude=' + latitude + '&longitude=' + longitude+'&treeType=CASTANEA')
+            .then((response) => response.json())
+            .then((featurecollection) => {
+                var annotations = featurecollection.features.map((feature) => {
+                    return {
+                        latitude: feature.geometry.coordinates[1],
+                        longitude: feature.geometry.coordinates[0],
+                        title: feature.properties.deuText+" - "+feature.properties.stUmfang
+                    }
+                });
+
+                this.setState({annotations});
+
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+    },
+
+    render() {
+        return (
+            <MapView
+                style={styles.map}
+                mapType="satellite"
+                showsUserLocation={true}
+                onRegionChange={this._onRegionChange}
+                onRegionChangeComplete={this._onRegionChangeComplete}
+                region={this.state.mapRegion || undefined}
+                annotations={this.state.annotations || undefined}
+                />
+        );
+    },
+
+    _getAnnotations(region) {
+        return [{
+            longitude: region.longitude,
+            latitude: region.latitude,
+            title: 'You Are Here',
+        }];
+    },
+
+    _onRegionChange(region) {
+    },
+
+    _onRegionChangeComplete(region) {
+        this.didGetPosition(region.latitude, region.longitude);
+    },
+
+    _onRegionInputChanged(region) {
+        this.setState({
+            mapRegion: region,
+            mapRegionInput: region,
+            annotations: this._getAnnotations(region),
+        });
+    },
+
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    map: {
+        flex: 1,
+        alignItems: "center",
+        borderColor: '#000000'
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    textInput: {
+        width: 150,
+        height: 20,
+        borderWidth: 0.5,
+        borderColor: '#aaaaaa',
+        fontSize: 13,
+        padding: 4,
+    },
+    changeButton: {
+        alignSelf: 'center',
+        marginTop: 5,
+        padding: 3,
+        borderWidth: 0.5,
+        borderColor: '#777777',
+    },
 });
 
-AppRegistry.registerComponent('kastanienApp', () => kastanienApp);
+
+AppRegistry.registerComponent('kastanienApp', () => MapViewExample);
---
+++ map.ios.js
@@ -0,0 +1,114 @@
+var MapRegionInput = React.createClass({
+
+    propTypes: {
+        region: React.PropTypes.shape({
+            latitude: React.PropTypes.number.isRequired,
+            longitude: React.PropTypes.number.isRequired,
+            latitudeDelta: React.PropTypes.number.isRequired,
+            longitudeDelta: React.PropTypes.number.isRequired,
+        }),
+        onChange: React.PropTypes.func.isRequired,
+    },
+
+    getInitialState: function() {
+        return {
+            region: {
+                latitude: 0,
+                longitude: 0,
+                latitudeDelta: 0,
+                longitudeDelta: 0,
+            }
+        };
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.setState({
+            region: nextProps.region || this.getInitialState().region
+        });
+    },
+
+    render: function() {
+        var region = this.state.region || this.getInitialState().region;
+        return (
+            <View>
+                <View style={styles.row}>
+                    <Text>
+                        {'Latitude'}
+                    </Text>
+                    <TextInput
+                        value={'' + region.latitude}
+                        style={styles.textInput}
+                        onChange={this._onChangeLatitude}
+                        selectTextOnFocus={true}
+                        />
+                </View>
+                <View style={styles.row}>
+                    <Text>
+                        {'Longitude'}
+                    </Text>
+                    <TextInput
+                        value={'' + region.longitude}
+                        style={styles.textInput}
+                        onChange={this._onChangeLongitude}
+                        selectTextOnFocus={true}
+                        />
+                </View>
+                <View style={styles.row}>
+                    <Text>
+                        {'Latitude delta'}
+                    </Text>
+                    <TextInput
+                        value={'' + region.latitudeDelta}
+                        style={styles.textInput}
+                        onChange={this._onChangeLatitudeDelta}
+                        selectTextOnFocus={true}
+                        />
+                </View>
+                <View style={styles.row}>
+                    <Text>
+                        {'Longitude delta'}
+                    </Text>
+                    <TextInput
+                        value={'' + region.longitudeDelta}
+                        style={styles.textInput}
+                        onChange={this._onChangeLongitudeDelta}
+                        selectTextOnFocus={true}
+                        />
+                </View>
+                <View style={styles.changeButton}>
+                    <Text onPress={this._change}>
+                        {'Change'}
+                    </Text>
+                </View>
+            </View>
+        );
+    },
+
+    _onChangeLatitude: function(e) {
+        regionText.latitude = e.nativeEvent.text;
+    },
+
+    _onChangeLongitude: function(e) {
+        regionText.longitude = e.nativeEvent.text;
+    },
+
+    _onChangeLatitudeDelta: function(e) {
+        regionText.latitudeDelta = e.nativeEvent.text;
+    },
+
+    _onChangeLongitudeDelta: function(e) {
+        regionText.longitudeDelta = e.nativeEvent.text;
+    },
+
+    _change: function() {
+        this.setState({
+            latitude: parseFloat(regionText.latitude),
+            longitude: parseFloat(regionText.longitude),
+            latitudeDelta: parseFloat(regionText.latitudeDelta),
+            longitudeDelta: parseFloat(regionText.longitudeDelta),
+        });
+        this.props.onChange(this.state.region);
+    },
+
+});
+
